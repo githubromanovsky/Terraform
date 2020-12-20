bdedad14f67baa491415a92c874532d0e33b575a
githubromanovsky <68157150+githubromanovsky@users.noreply.github.com>
2020-12-20 17:20:48 +0300

    Create acl.tf

---
+++ acl.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "my_sg" {
+  name        = "my_sg"
+  description = "Security Group for access EC2."
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
